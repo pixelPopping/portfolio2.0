c783b784cc0695cc9b61124720d682831dd2bb2a
Yorian Fransz <sydney-cook@outlook.com>
2026-09-07 10:53:51 +0200

    wijzeging

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -6,6 +6,7 @@ import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
 
+
 function App() {
   return (
     <div className={styles.app}>
