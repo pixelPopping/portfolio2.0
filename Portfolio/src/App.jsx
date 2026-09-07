e412e2ae75aafe0013b6e5fe9822ebc128e738b0
Yorian Fransz <sydney-cook@outlook.com>
2026-09-07 10:56:36 +0200

    verandering

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -6,7 +6,6 @@ import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
 
-
 function App() {
   return (
     <div className={styles.app}>
